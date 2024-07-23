fdc26ce914c46da603a346ed529fb707eed2fb69
Lucas Bueno <lucas_bueno3@hotmail.com>
2024-07-22 23:40:14 -0300

    Initial commit

---
+++ GithubStatistics/Data/RankingItem.cs
@@ -0,0 +1,9 @@
+﻿namespace GithubStatistics.Data
+{
+    public class RankingItem
+    {
+        public int Position { get; set; }
+        public string AvatarUrl { get; set; }
+        public string Name { get; set; }
+    }
+}
